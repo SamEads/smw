5426d940b057b3c19e124622b4b00a5241bcfaea
Sam <eads.samuel@gmail.com>
2026-09-09 18:24:36 -0400

    remove session, .DS_Store

--- src/main.cpp
+++ src/main.cpp
@@ -48,88 +48,12 @@ int main()
 		{ '6', 6 }, { '7', 7 }, { '8', 8 },
 		{ '9', 9 },
 	});
-	
-	/*
-	float scrollValueX = 0;
-	float scrollValueY = 0;
-	while (window.isOpen())
-	{
-		Keys::update(window.hasFocus());
-		while (const std::optional event = window.pollEvent())
-		{
-			if (const auto* scroll = event->getIf<sf::Event::MouseWheelScrolled>())
-			{
-				float delta = scroll->delta;
 
-				if (Keys::held(sf::Keyboard::Scancode::LShift))
-					scrollValueX += delta * 2.0f;
-				else
-					scrollValueY += delta * 2.0f;
-			}
-			if (event->is<sf::Event::Closed>())
-			{
-				window.close();
-			}
-			else if (const auto* resized = event->getIf<sf::Event::Resized>())
-			{
-				window.setView(sf::View(
-					sf::FloatRect(
-						{ 0.f, 0.f },
-						{ static_cast<float>(resized->size.x),
-						static_cast<float>(resized->size.y) }
-					)
-				));
-			}
-		}
-		window.clear();
-		sf::Color topGradient(0, 0, 96);
-		sf::Color bottomGradient(0, 0, 255);
-		sf::VertexArray background(sf::PrimitiveType::Triangles, 6);
-
-		for (int i = 0; i < 6; ++i)
-		{
-			background[0].position = sf::Vector2f(0, 0);
-			background[1].position = sf::Vector2f(window.getSize().x, 0);
-			background[2].position = sf::Vector2f(window.getSize().x, window.getSize().y);
-			background[3].position = sf::Vector2f(window.getSize().x, window.getSize().y);
-			background[4].position = sf::Vector2f(0, window.getSize().y);
-			background[5].position = sf::Vector2f(0, 0);
-
-			float factor = background[i].position.y / window.getSize().y;
-
-			background[i].color = sf::Color(
-				static_cast<uint8_t>((1.0f - factor) * topGradient.r + factor * bottomGradient.r),
-				static_cast<uint8_t>((1.0f - factor) * topGradient.g + factor * bottomGradient.g),
-				static_cast<uint8_t>((1.0f - factor) * topGradient.b + factor * bottomGradient.b)
-			);
-		}
-		sf::Sprite durr(Textures::get("tiles/ground.png"));
-		durr.setScale({ 2, 2 });
-		durr.setPosition({ scrollValueX * 32, scrollValueY * 32 });
-		window.draw(background);
-		window.draw(durr);
-		Font::SMALL.draw("TILESET NO. 0", window, 0, 0);
-
-		sf::RectangleShape red({ 32, 32 });
-		red.setFillColor({ 255, 0, 0, 255 });
-		auto mousePos = sf::Mouse::getPosition(window);
-		mousePos.x = floorf(mousePos.x / 32.0f) * 32.0f;
-		mousePos.y = floorf(mousePos.y / 32.0f) * 32.0f;
-		red.setPosition({ mousePos.x, mousePos.y });
-		sf::RenderStates states;
-		states.blendMode = sf::BlendAdd;
-
-		window.draw(red, states);
-
-		window.display();
-	}
-	*/
-
-	// sf::Music music("assets/music/baci_perugina.ogg");
-	// music.setVolume(90.0f);
-	// music.setPitch(1.1f);
-	// music.setLooping(true);
-	// music.play();
+	sf::Music music("assets/music/baci_perugina.ogg");
+	music.setVolume(90.0f);
+	music.setPitch(1.1f);
+	music.setLooping(true);
+	music.play();
 	while (window.isOpen())
 	{
 		while (const std::optional event = window.pollEvent())
